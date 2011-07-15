31baa8188c11fa27b38711d47154d791f2b4f8c8
ardatdat <ardatdat@gmail.com>
2011-07-15 20:31:46 +0800

    overclock: OC to 1.2GHz

--- makekernel.sh
+++ makekernel.sh
@@ -5,8 +5,11 @@ PATH=$PATH:/home/yiudou/ndk/toolchains/arm-eabi-4.4.0/prebuilt/linux-x86/bin
 export ARCH=arm
 export CROSS_COMPILE=arm-eabi-
 
-make tegra_defconfig
+###make tegra_defconfig
 
-make -j`grep 'processor' /proc/cpuinfo | wc -l`
+make -j2
+
+
+cp ./arch/arm/boot/zImage  /home/yiudou/kitchen168/BOOT-EXTRACTED/
 
 

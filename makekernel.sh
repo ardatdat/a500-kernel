5b2a6cccae7d743d2fa2220b92bb969a132150d7
ardatdat <ardatdat@gmail.com>
2011-07-15 16:02:24 +0800

    make: modify the makekernel.sh to fit to machine's dev. environment

--- makekernel.sh
+++ makekernel.sh
@@ -1,5 +1,12 @@
 #/usr/bin
 
-export CCOMPILER=~/CM/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
-make ARCH=arm CROSS_COMPILE=$CCOMPILER oldconfig
-make ARCH=arm CROSS_COMPILE=$CCOMPILER -j`grep 'processor' /proc/cpuinfo | wc -l`
+PATH=$PATH:/home/yiudou/ndk/toolchains/arm-eabi-4.4.0/prebuilt/linux-x86/bin
+
+export ARCH=arm
+export CROSS_COMPILE=arm-eabi-
+
+make tegra_defconfig
+
+make -j`grep 'processor' /proc/cpuinfo | wc -l`
+
+

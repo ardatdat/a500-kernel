aa4ba1529625474101630aca62f9e1a46a014142
ardatdat <ardatdat@gmail.com>
2011-07-17 18:43:19 +0800

    autogroup scheduler patch. thx Cybertronic & bigrushdog

--- makekernel.sh
+++ makekernel.sh
@@ -5,9 +5,9 @@ PATH=$PATH:/home/yiudou/ndk/toolchains/arm-eabi-4.4.0/prebuilt/linux-x86/bin
 export ARCH=arm
 export CROSS_COMPILE=arm-eabi-
 
-###make tegra_defconfig
+make tegra_defconfig
 
-make -j2
+make -j3 -B
 
 
 cp ./arch/arm/boot/zImage  /home/yiudou/kitchen168/BOOT-EXTRACTED/

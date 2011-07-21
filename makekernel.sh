ea345104af5fd5573d44ade80730393d6dca6a47
ardatdat <ardatdat@gmail.com>
2011-07-21 09:56:52 +0800

    compile: use arm-eabi-4.4.3

--- makekernel.sh
+++ makekernel.sh
@@ -1,6 +1,8 @@
 #/usr/bin
 
-PATH=$PATH:/home/yiudou/ndk/toolchains/arm-eabi-4.4.0/prebuilt/linux-x86/bin
+###PATH=$PATH:/home/yiudou/ndk/toolchains/arm-eabi-4.4.0/prebuilt/linux-x86/bin
+
+PATH=$PATH:/media/86GB/ginger_src/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin
 
 export ARCH=arm
 export CROSS_COMPILE=arm-eabi-

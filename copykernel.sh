bdf99247e799f5b0ad1624810c2aeb109a3924b0
ardatdat <ardatdat@gmail.com>
2011-07-22 19:16:15 +0800

    Initial commit

---
+++ copykernel.sh
@@ -0,0 +1,4 @@
+
+cp ./arch/arm/boot/zImage  /home/yiudou/kitchen168/BOOT-EXTRACTED/
+cp ./drivers/scsi/scsi_wait_scan.ko /home/yiudou/kitchen168/WORKING_081310_012723/system/lib/modules/
+cp ./drivers/net/wireless/bcm4329/bcm4329.ko /home/yiudou/kitchen168/WORKING_081310_012723/system/lib/modules/

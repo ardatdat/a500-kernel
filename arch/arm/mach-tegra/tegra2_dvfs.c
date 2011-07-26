0f60aae692e4540d83ee44b9a21a30beeacf58b8
ardatdat <ardatdat@gmail.com>
2011-07-27 02:42:25 +0800

    OC: 2nd try to OC to 1.4 Ghz

--- arch/arm/mach-tegra/tegra2_dvfs.c
+++ arch/arm/mach-tegra/tegra2_dvfs.c
@@ -47,20 +47,20 @@ static const int cpu_millivolts[MAX_DVFS_FREQS] =
 
 static const int cpu_speedo_nominal_millivolts[] =
 /* spedo_id  0,    1,    2 */
-	{ 1100, 1025, 1125 };
+	{ 1100, 1175, 1125 };
 
 static const int core_speedo_nominal_millivolts[] =
 /* spedo_id  0,    1,    2 */
-	{ 1225, 1225, 1300 };
+	{ 1225, 1275, 1300 };
 
 #define KHZ 1000
 #define MHZ 1000000
 
 static struct dvfs_rail tegra2_dvfs_rail_vdd_cpu = {
 	.reg_id = "vdd_cpu",
-	.max_millivolts = 1125,
+	.max_millivolts = 1225,
 	.min_millivolts = 750,
-	.nominal_millivolts = 1125,
+	.nominal_millivolts = 1225,
 };
 
 static struct dvfs_rail tegra2_dvfs_rail_vdd_core = {
@@ -68,9 +68,9 @@ static struct dvfs_rail tegra2_dvfs_rail_vdd_core = {
 ///	.max_millivolts = 1275,
 ///	.min_millivolts = 950,
 ///	.nominal_millivolts = 1200,
-	.max_millivolts = 1300,
+	.max_millivolts = 1275,
 	.min_millivolts = 950,
-	.nominal_millivolts = 1225,
+	.nominal_millivolts = 1200,
 	.step = 150, /* step vdd_core by 150 mV to allow vdd_aon to follow */
 };
 
@@ -79,9 +79,9 @@ static struct dvfs_rail tegra2_dvfs_rail_vdd_aon = {
 ///	.max_millivolts = 1275,
 ///	.min_millivolts = 950,
 ///	.nominal_millivolts = 1200,
-	.max_millivolts = 1300,
+	.max_millivolts = 1275,
 	.min_millivolts = 950,
-	.nominal_millivolts = 1225,
+	.nominal_millivolts = 1200,
 #ifndef CONFIG_TEGRA_CORE_DVFS
 	.disabled = true,
 #endif

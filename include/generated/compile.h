37d05648871c9b73d664e362bd81fae6f5d593cf
ardatdat <ardatdat@gmail.com>
2011-07-28 04:01:58 +0800

    Initial commit: official honeycomb 3.1 kernel

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 7 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#7 SMP PREEMPT Thu Jul 28 03:15:08 HKT 2011"
+#define LINUX_COMPILE_TIME "03:15:08"
+#define LINUX_COMPILE_BY "yiudou"
+#define LINUX_COMPILE_HOST "yiudou-desktop"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
